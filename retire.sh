722630efe2213728ebca866d34da0af775a370f1
S. Karatkevich <root@kevit.info>
2023-02-01 16:49:06 +0100

    fixing cleaning

--- retire.sh
+++ retire.sh
@@ -3,6 +3,10 @@ podman stop server01
 podman stop server02
 podman rmi ansible-control
 podman rmi ansible-centos
+podman rm server01
+podman rm server02
+podman rm control
+podman network rm ansible
 rm -rf .ssh
 rm key key.pub
 rm -rf .ansible
